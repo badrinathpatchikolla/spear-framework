432d5d058db65b7d99bb8659a7450d7e451845ee
AnudeepKonaboina <59328701+AnudeepKonaboina@users.noreply.github.com>
2021-04-28 00:13:21 +0530

    fixed bugs in setup.sh

--- setup.sh
+++ setup.sh
@@ -29,6 +29,6 @@ sleep 60
 #starting hadoop
 echo "Starting services"
 docker exec -it spark bash -c "hdfs namenode -format && start-dfs.sh && hdfs dfs -mkdir -p /tmp && hdfs dfs -mkdir -p /user/hive/warehouse && hdfs dfs -chmod g+w /user/hive/warehouse" &&
-docker exec -d spark bash -c "hive --service metastore && hive --service hiveserver2"
-docker exec -it spark bash -c "curl https://bintray.com/sbt/rpm/rpm |  tee /etc/yum.repos.d/bintray-sbt-rpm.repo && yum install -y sbt && cd /opt && git clone https://github.com/AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt package"
-docker exec -it spark bash -c "cd /opt/spear-framework/target/scala-2.12 && spark-shell --jars spear-framework_2.12-0.1.jar --packages org.apache.spark:spark-hive_2.11:2.4.0"
+  docker exec -d spark bash -c "hive --service metastore && hive --service hiveserver2" &&
+  docker exec -it spark bash -c "curl https://bintray.com/sbt/rpm/rpm |  tee /etc/yum.repos.d/bintray-sbt-rpm.repo && yum install -y sbt && cd /opt && git clone https://github.com/AnudeepKonaboina/spear-framework.git && cd spear-framework && sbt package" &&
+  docker exec -it spark bash -c "cd /opt/spear-framework/target/scala-2.12 && spark-shell --jars spear-framework_2.12-0.1.jar --packages org.apache.spark:spark-hive_2.11:2.4.0"
